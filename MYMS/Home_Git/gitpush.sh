dbe53755fb84cc39bbe24e58500fe7178b06e9ae
MYMS2020 <2112717099@qq.com>
2020-08-06 19:28:58 +0800

    Auto push to git

--- MYMS/Home_Git/gitpush.sh
+++ MYMS/Home_Git/gitpush.sh
@@ -2,9 +2,23 @@
 #git config --global user.name "MYMS2020"
 #git config --global user.email "2112717099@qq.com" 
 #git remote add origen git@github.com:MYMS2020/MYMS2020.github.io 
+git branch
 git add .
+git status
 git commit -m "Auto push to git"
 git push -u origin master:master
+echo 9
+sleep 1
+echo 8
+sleep 1
+echo 7
+sleep 1
+echo 6
+sleep 1
+echo 5
+sleep 1
+echo 4
+sleep 1
 echo 3
 sleep 1
 echo 2
